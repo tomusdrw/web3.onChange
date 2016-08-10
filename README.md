fe02c780994a96d7552b4473820b1a3332cca81d
Tomusdrw <tomusdrw@gmail.com>
2016-08-10 17:12:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+[![NPM version][npm-image]][npm-url] 
+[![Build Status][travis-image]][travis-url]
+[![Join the chat at https://gitter.im/ethcore/parity][gitter-image]][gitter-url]
+[![GPLv3][license-image]][license-url]
+
+[npm-image]: https://badge.fury.io/js/web3.onchange.png
+[npm-url]: https://npmjs.org/package/web3.onchange
+[travis-image]: https://travis-ci.org/tomusdrw/web3.onChange.svg?branch=master
+[travis-url]: https://travis-ci.org/tomusdrw/web3.onChange
+[gitter-image]: https://badges.gitter.im/Join%20Chat.svg
+[gitter-url]: https://gitter.im/ethcore/parity?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
+[license-image]: https://img.shields.io/badge/license-GPL%20v3-green.svg
+[license-url]: http://www.gnu.org/licenses/gpl-3.0.en.html
+
+# Web3.onChange
+
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const webpack = require('webpack');
+const CopyWepbackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const ENV = process.env.NODE_ENV || 'development';
+const isProd = ENV === 'production';
+
+module.exports = {
+  debug: !isProd,
+  cache: !isProd,
+  devtool: isProd ? '#source-map' : '#cheap-module-eval-source-map',
+  context: path.join(__dirname, './lib'),
+  entry: {
+    'index': './index.js'
+  },
+  output: {
+    library: 'web3.onchange',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    path: isProd ? path.join(__dirname, './dist') : path.join(__dirname, './build'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.json/,
+        loader: 'json'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  plugins: (function () {
+    var plugins = [
+      new CopyWepbackPlugin([{ from: './index.html '}]),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(ENV)
+        }
+      }),
+    ];
+
+    if (isProd) {
+      plugins.push(new webpack.optimize.DedupePlugin());
+      plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+    }
+
+    return plugins;
+  }()),
+  devServer: {
+    contentBase: './build',
+    hot: !isProd,
+    proxy: {
+      '/rpc*': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
+};
